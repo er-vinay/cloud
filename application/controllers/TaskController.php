bf592792e215784c37548f03a8960cfdfc924367
er-vinay <vinaykumarfd@gmail.com>
2021-11-02 15:09:03 +0530

    Leads count check

--- application/controllers/TaskController.php
+++ application/controllers/TaskController.php
@@ -31,14 +31,15 @@
 
 		public function index($stage)
 		{
+        	$this->load->library("pagination");
 	        $url = (base_url() . $this->uri->segment(1) ."/". $this->uri->segment(2));
 		    $conditions = "LD.company_id='". company_id ."' AND LD.product_id='". product_id ."' AND LD.stage='". $stage ."'";
 
 	        $config = array();
 	        $config["base_url"] = $url;
-	        $config["total_rows"] 		= $this->Tasks->getLeadsCount($stage); // get count leads
-	        $config["per_page"] 		= 10;
-	        $config["uri_segment"] 		= 3;
+	        $config["total_rows"] = $this->Tasks->getLeadsCount($stage); // get count leads
+	        $config["per_page"] = 10;
+	        $config["uri_segment"] = 3;
 	        $config['full_tag_open']    = '<div class="pagging text-right"><nav><ul class="pagination">';
 	        $config['full_tag_close']   = '</ul></nav></div>';
 	        $config['num_tag_open']     = '<li class="page-item"><span class="page-link">';
@@ -55,17 +56,16 @@
 	        $config['last_tag_close']  = '</span></li>';
 
 	        $this->pagination->initialize($config);
-	        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : "0";
+	        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
 	        $data['links'] = $this->pagination->create_links();
 	        $data['pageURL'] = $url;
 
-			if($this->uri->segment(1) == "holdleads" || $this->uri->segment(1) == "applicationHold") {
-	        	$data['leadDetails'] = $this->Tasks->holdleads($conditions, $page, $config["per_page"]);
-			}else{
-        		$data['leadDetails'] = $this->Tasks->index($conditions, $page, $config["per_page"]);
-			}
 
-			// echo "<pre>"; print_r($this->db->last_query()); exit;
+			if($this->uri->segment(1) == "holdleads" || $this->uri->segment(1) == "applicationHold") {
+	        	$data['leadDetails'] = $this->Tasks->holdleads($conditions, $config["per_page"], $page);
+			}else{
+        		$data['leadDetails'] = $this->Tasks->index($conditions, $config["per_page"], $page);
+			}
 			// echo "<pre>". $conditions. ', per_page : '. $config["per_page"]. ', page: '. $page. ', '; print_r($data['leadDetails']->num_rows());
 
 			// echo $data['links'];
