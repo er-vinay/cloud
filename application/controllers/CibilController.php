b567cc10998690ad009b8ac2e1a314b549f6c480
er-vinay <vinaykumarfd@gmail.com>
2021-11-02 16:42:19 +0530

    Leads check cibil integration

--- application/controllers/CibilController.php
+++ application/controllers/CibilController.php
@@ -22,11 +22,11 @@
                 $query = $this->Tasks->join_two_table_with_where($conditions, $fetch, $table1, $table2, $join2);
                 $leadDetails = $query->row();
 
-                $f_name = (($leadDetails->first_name != '-') || ($leadDetails->first_name != '')) : $leadDetails->first_name : '';
-                $m_name = (($leadDetails->middle_name != '-') || ($leadDetails->middle_name != '')) : $leadDetails->middle_name : '';
-                $s_name = (($leadDetails->sur_name != '-') || ($leadDetails->sur_name != '')) : $leadDetails->sur_name : '';
+                // $f_name = (($leadDetails->first_name != '-') || ($leadDetails->first_name != '')) : $leadDetails->first_name : '';
+                // $m_name = (($leadDetails->middle_name != '-') || ($leadDetails->middle_name != '')) : $leadDetails->middle_name : '';
+                // $s_name = (($leadDetails->sur_name != '-') || ($leadDetails->sur_name != '')) : $leadDetails->sur_name : '';
 
-                $name = $f_name ." ". $m_name ." ". $s_name;
+                // $name = $f_name ." ". $m_name ." ". $s_name;
 
                 // if($leadDetails->check_cibil_status == 0)
                 if($leadDetails->check_cibil_status == 0)
@@ -34,7 +34,7 @@
                     if(!empty($lead_id))
                     {
                         $customer_id= $leadDetails->customer_id;
-                        $name       = $name;
+                        $name       = $leadDetails->first_name;
                         $mobile     = $leadDetails->mobile;
                         $pancard    = $leadDetails->pancard;
                         $gender     = $leadDetails->gender;
