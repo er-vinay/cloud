f8d6b6572b0b1b8f9b69e61ebf4f5ca328db1d17
er-vinay <vinaykumarfd@gmail.com>
2021-11-02 15:53:59 +0530

    Leads count check

--- application/controllers/TaskController.php
+++ application/controllers/TaskController.php
@@ -36,21 +36,35 @@
 		    $conditions = "LD.company_id='". company_id ."' AND LD.product_id='". product_id ."' AND LD.stage='". $stage ."'";
 
 		    $config = array();
-       $config["base_url"] = $url;
-       $config["total_rows"] = $this->Tasks->getLeadsCount($stage);
-       $config["per_page"] = 5;
-       $config["uri_segment"] = 3;
-       $this->pagination->initialize($config);
-       $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
-        $data['pageURL'] = $url;
+			$config["base_url"] = $url;
+			$config["total_rows"] = $this->Tasks->getLeadsCount($stage);
+			$config["per_page"] = 5;
+			$config["uri_segment"] = 3;
+	        $config['full_tag_open']    = '<div class="pagging text-right"><nav><ul class="pagination">';
+	        $config['full_tag_close']   = '</ul></nav></div>';
+	        $config['num_tag_open']     = '<li class="page-item"><span class="page-link">';
+	        $config['num_tag_close']    = '</span></li>';
+	        $config['cur_tag_open']     = '<li class="page-item active"><span class="page-link">';
+	        $config['cur_tag_close']    = '<span class="sr-only">(current)</span></span></li>';
+	        $config['next_tag_open']    = '<li class="page-item"><span class="page-link">';
+	        $config['next_tag_close']  = '<span aria-hidden="true"></span></span></li>';
+	        $config['prev_tag_open']    = '<li class="page-item"><span class="page-link">';
+	        $config['prev_tag_close']  = '</span></li>';
+	        $config['first_tag_open']   = '<li class="page-item"><span class="page-link">';
+	        $config['first_tag_close'] = '</span></li>';
+	        $config['last_tag_open']    = '<li class="page-item"><span class="page-link">';
+	        $config['last_tag_close']  = '</span></li>';
+			$this->pagination->initialize($config);
+			$page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+	        $data['pageURL'] = $url;
 
 
-		if($this->uri->segment(1) == "holdleads" || $this->uri->segment(1) == "applicationHold") {
-        	$data['leadDetails'] = $this->Tasks->holdleads($conditions, $config["per_page"], $page);
-		}else{
-    		$data['leadDetails'] = $this->Tasks->index($conditions, $config["per_page"], $page);
-		}
-       $data["links"] = $this->pagination->create_links();
+			if($this->uri->segment(1) == "holdleads" || $this->uri->segment(1) == "applicationHold") {
+	        	$data['leadDetails'] = $this->Tasks->holdleads($conditions, $config["per_page"], $page);
+			}else{
+	    		$data['leadDetails'] = $this->Tasks->index($conditions, $config["per_page"], $page);
+			}
+       		$data["links"] = $this->pagination->create_links();
 
 
 
