10f2ca80792fdb1f54445db1d2f80aeeec36dc87
er-vinay <vinaykumarfd@gmail.com>
2021-11-02 17:56:49 +0530

    Leads check cibil integration

--- application/controllers/CibilController.php
+++ application/controllers/CibilController.php
@@ -22,7 +22,6 @@
                 $query = $this->Tasks->join_two_table_with_where($conditions, $fetch, $table1, $table2, $join2);
                 $leadDetails = $query->row();
 
-                echo "<pre>"; print_r($leadDetails); exit;
 
                 // if($leadDetails->check_cibil_status == 0)
                 // {
