9f241cda5ee8c7aba34f106c4a317da1c68b6960
er-vinay <vinaykumarfd@gmail.com>
2021-11-10 17:35:17 +0530

    Cibil amount chenges

--- application/controllers/CibilController.php
+++ application/controllers/CibilController.php
@@ -481,21 +481,23 @@
             $overDueAmount = 0;
             $zeroBalance = 0;
             $i = 0;
+            
             foreach($summary->children() as $key =>$child) {
-                // echo $i. "summary node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
                 if($i == 6){
+            // echo $i. "summary node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
                     $totalAccount = $child;
                 } else if($i == 7){
+            // echo $i. "summary node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
                     $totalBalance = $child;
                 } else if($i == 8){
+            // echo $i. "summary node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
                     $overDueAccount = $child;
                 }
                 $i++;
             }
-
             foreach($overdue->children() as $key =>$child) {
                 if($i == 1){
-                    // echo $i. "overdue node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
+            // echo $i. "overdue node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
                     $overDueAmount = $child;
                 }
                 $i++;
@@ -577,20 +579,20 @@
                 $i = 0;
                 foreach($overdue->children() as $key =>$child) {
                     if($i == 1){
-                echo $i. "overdue node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
+                // echo $i. "overdue node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
                         $overDueAmount = $child;
                     }
                     $i++;
                 }
                 foreach($summary->children() as $key =>$child) {
                     if($i == 6){
-                echo $i. "summary node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
+                // echo $i. "summary node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
                         $totalAccount = $child;
                     } else if($i == 7){
-                echo $i. "summary node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
+                // echo $i. "summary node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
                         $totalBalance = $child;
                     } else if($i == 8){
-                echo $i. "summary node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
+                // echo $i. "summary node: " . $child->getName(). " = ". $key ." val : " . $child . "</br>";
                         $overDueAccount = $child;
                     }
                     $i++;
