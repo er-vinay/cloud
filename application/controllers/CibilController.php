232d11774712d87b46a15546decf7130ea42b21e
er-vinay <vinaykumarfd@gmail.com>
2021-11-10 15:09:50 +0530

    Cibil amount chenges

--- application/controllers/CibilController.php
+++ application/controllers/CibilController.php
@@ -619,7 +619,7 @@
                 libxml_use_internal_errors(true);
                 $xml = simplexml_load_string($result); //or simplexml_load_file
 
-                echo "<pre>"; print_r($result); exit;
+                // echo "<pre>"; print_r($result); exit;
                 foreach( libxml_get_errors() as $error ) {
                     print_r($error);
                 }
@@ -629,7 +629,7 @@
                     throw new Exception('Input string could not be converted.');
                 }
                 $xml = simplexml_load_string( $result) or die("xml not loading");
-                echo '<pre>'; print_r($xml->body->table->tr[29]->td->table->tr[4]['VarNum']); exit;
+                echo '<pre>'; print_r($xml->body->table->tr[29]->td->table->tr[4]->td[1]['BlackLabel padAll5']); exit;
                 
                 // $data = [
                 //     'memberCode'     => $xml->body->table->tr[1]->td->table->tr[1]->td[0]->table->tr[1]->td[1],
